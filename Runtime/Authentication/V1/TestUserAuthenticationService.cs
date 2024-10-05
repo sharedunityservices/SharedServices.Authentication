0c37ea0075fe359d6d567fed7d1af41e8c9631f1
ETdoFresh <ETdoFresh@gmail.com>
2024-10-04 23:42:22 -0500

    Update TestUserAuthenticationService for improved consistency
    This change modifies the initialization values for the UserId and Token properties in the TestUserAuthenticationService class. The default identifiers have been updated to use a more consistent placeholder format, changing from numeric sequences to a uniform dashed structure. Additionally, the Authenticate method now converts the username to lowercase when constructing the UserId and Token, ensuring uniformity in identifier generation across different invocations. This update enhances the consistency and predictability of user identifiers in test environments.

--- Runtime/Authentication/V1/TestUserAuthenticationService.cs
+++ Runtime/Authentication/V1/TestUserAuthenticationService.cs
@@ -8,16 +8,16 @@ namespace SharedServices.Authentication.V1
     public class TestUserAuthenticationService : IAuthenticationService
     {
         public bool IsAuthenticated { get; private set; }
-        public string UserId { get; private set; } = "00000000-test-user-id-000000000000";
+        public string UserId { get; private set; } = "----test-user-id----";
         public string Username { get; private set; } = "test-user";
-        public string Token { get; private set; } = "00000000-test-user-token-000000000000";
+        public string Token { get; private set; } = "----test-user-id----";
 
         public void Authenticate(string username, string password, Action callback)
         {
             IsAuthenticated = true;
-            UserId = $"00000000-{username}-id-000000000000";
+            UserId = $"----{username.ToLower()}----";
             Username = username;
-            Token = $"00000000-{username}-token-000000000000";
+            Token = $"----{username.ToLower()}----";
         }
 
         public async Task AuthenticateAsync(string username, string password)
